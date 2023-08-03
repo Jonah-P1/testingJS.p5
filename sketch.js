de0c504acc5a033bede0b5675a24bbad6cf91d28
Jonah-P1 <jonahpk781@gmail.com>
2023-08-03 17:31:55 +0200

    New feature allows new squares

--- sketch.js
+++ sketch.js
@@ -1,23 +1,55 @@
 function setup() {
   frameRate(60);
-  createCanvas(1000, 1000, WEBGL);
-  noFill();
-  console.log("Hallo");
-  multipleSquares();
-  draw();
+  createCanvas(720, 720);
+  fullSquares(16);
 }
 
+/*
 function draw() {
   noFill();
   strokeWeight(1);
   rect(60, 60, 20, 20);
 }
+*/
 
 function multipleSquares() {
-  for (var i = 0; i <= 14; i++) {
+  for (var x = 0; x <= 11; x++) {
     fill(256);
     strokeWeight(1);
-    rect(i * 20, 0, 20, 20);
-    console.log(i);
+    rect(x * 20, 0, 20, 20);
+    console.log(x);
+  }
+}
+
+/*
+The function above generates twelve squares (20x20 pixels) in a line following the x - axis
+*/
+
+function moreSquares() {
+  for (var y = 0; y <= 11; y++) {
+    fill(256);
+    strokeWeight(1);
+    rect(0, y * 20, 20, 20);
+    console.log(y);
+  }
+}
+
+/*
+The function above is derived from multipleSquares() and generates twelve
+20x20 squares in a line following the y - axis
+*/
+
+function fullSquares(maxheight) {
+  for (var y = 0; y <= maxheight; y++) {
+    let valueToAdd = y * 20;
+    fill(256);
+    strokeWeight(1);
+    for (var x = 0; x <= 11; x++) {
+      fill(256);
+      strokeWeight(1);
+      rect(x * 20, valueToAdd, 20, 20);
+      console.log(x);
+    }
+    console.log(y);
   }
 }
