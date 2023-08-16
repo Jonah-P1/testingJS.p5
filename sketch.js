b10cc8d0f2955582440a1651e32f9c7e23a83ee9
Jonah-P1 <jonahpk781@gmail.com>
2023-08-16 11:37:27 +0200

    reworked gradient feature and added animation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
-var full_squares_max_width = 50;
-var full_squares_max_height = 50;
-var full_squares_X1 = 5;
-var full_squares_Y1 = 5; //global variables
+const full_squares_max_width = 31;
+const full_squares_max_height = 47;
+const Y_shift = 30;
+const full_squares_X1 = 10;
+const full_squares_Y1 = 10; //variable to deterrmine the size of the rectangle and its squares
 
 function multipleSquares() {
   for (var x = 0; x <= 11; x++) {
@@ -29,16 +30,21 @@ function fullSquares() {
   var max_height = full_squares_max_height;
   var X1 = full_squares_X1;
   var Y1 = full_squares_Y1;
-  for (var y = 0; y <= max_height; y++) {
+  let animation = frameCount * 0.5; //parameter for animation
+  for (let y = 0; y <= max_height; y++) {
     let value_Y_to_add = y * Y1;
-    let v3 = y * 0.5; //gradient??
-    let t = frameCount * 0.5; //fade
+    let gradient = map(y, 0, max_height, 210, 270); //animated gradient effect
+    //stroke(45, 70, 100);
     strokeWeight(1);
-    for (var x = 0; x <= max_width; x++) {
+    for (let x = 0; x <= max_width; x++) {
       let value_X_to_add = x * X1;
-      strokeWeight(1);
-      fill(180 + v3, 60 + v3, t);
-      //colorcreationHSB(180, 80, 60);
+      fill(gradient, 95, 75);
+      if (x % 2 === 0) {
+        value_Y_to_add += Y1 + Y_shift;
+      } else {
+        value_Y_to_add -= Y1 + Y_shift;
+      }
+      //if conditional creates visual effect of columns shifting
       rect(
         width / 2 + value_X_to_add - global_total_square_width / 2,
         height / 2 + value_Y_to_add - global_total_square_height / 2,
@@ -58,6 +64,7 @@ function squareDimensions() {
   console.log(total_square_width, total_square_height);
   return [total_square_width, total_square_height];
 }
+//determines the size of the rectangle created through fullSquares()
 
 function randomColor() {
   var random_value1 = floor(random(210, 187));
@@ -68,18 +75,7 @@ function randomValue() {
   var random_value2 = floor(random(80, 100));
   return random_value2;
 }
-/*
-function colorcreationHSB(hue, sat, bright) {
-  for (var r = 1; r <= 15; r++) {
-    let g = r * 2;
-    let t = frameCount * 0.01;
-    let value_hue_to_add = hue * g;
-    let value_saturation_to_add = sat * g;
-    let value_brightness_to_add = bright + t;
-    fill(value_hue_to_add, value_saturation_to_add, value_brightness_to_add);
-  }
-} //function defines the color of the full square in HSB
-*/
+
 function interference() {
   fill(0, 0, 0);
   moreSquares(6);
@@ -87,19 +83,14 @@ function interference() {
 
 function setup() {
   createCanvas(800, 800);
-  frameRate(60);
-  randomColor();
-  let random_color = randomColor();
-  randomValue();
-  let random_value = randomValue();
-
+  //frameRate(30);
+  noLoop();
+  noStroke();
   console.log(
     full_squares_max_width,
     full_squares_max_height,
     full_squares_X1,
-    full_squares_Y1,
-    random_color,
-    random_value
+    full_squares_Y1
   );
   squareDimensions();
   let global_square_dimensions = squareDimensions();
@@ -113,8 +104,9 @@ function setup() {
 }
 
 function draw() {
-  background(210, 0, 50);
-  noStroke();
+  //background(210, 0, 50);
   fullSquares();
   //interference();
 }
+
+// p.93
